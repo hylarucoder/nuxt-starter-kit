c052aa209accaf7575ee2b0f992fc2f7cfcb5492
twocucao <twocucao@gmail.com>
2023-09-06 23:07:05 +0800

    embed

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="w-full px-5 pb-5 sm:px-40 sm:pt-10">
       <div class="flex w-full flex-col sm:flex-row">
         <div class="mt-4 pr-5">
-          {{ t("Home.Title") }}
+          home
         </div>
       </div>
     </div>
@@ -23,7 +23,6 @@
   </NuxtLayout>
 </template>
 <script lang="ts" setup>
-const { t } = useTrans()
 const { data, status, getCsrfToken, getProviders } = useAuth()
 definePageMeta({ auth: false })
 
